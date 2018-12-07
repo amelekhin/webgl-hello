db84df256fb2a62c7a8a99e7e0eb66bf9fcdd1a6
Anton Amelekhin <a.amelekhin@outlook.com>
2018-12-07 23:41:33 +0300

    Replace triangle with random rectangles

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
 import { createShader, createProgram, updateCanvasSize } from "./utils";
 
+function randomInt(range: number) {
+    return Math.floor(Math.random() * range);
+}
+
+function setRectangle(gl: WebGL2RenderingContext, x: number, y: number, width: number, height: number) {
+    const x1 = x;
+    const x2 = x + width;
+    const y1 = y;
+    const y2 = y + height;
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+        x1, y1,
+        x2, y1,
+        x1, y2,
+        x1, y2,
+        x2, y1,
+        x2, y2
+    ]), gl.STATIC_DRAW);
+}
+
 function main() {
     const canvas = document.getElementById("canvas") as HTMLCanvasElement;
     updateCanvasSize(canvas);
@@ -14,40 +34,35 @@ function main() {
     const vertShaderSrc: string = require("./shaders/shader.vert");
     const fragShaderSrc: string = require("./shaders/shader.frag");
 
-    const vertShader = createShader(gl, gl.VERTEX_SHADER, vertShaderSrc);
-    const fragShader = createShader(gl, gl.FRAGMENT_SHADER, fragShaderSrc);
-    const program = createProgram(gl, vertShader, fragShader);
+    const vertShader  = createShader(gl, gl.VERTEX_SHADER, vertShaderSrc);
+    const fragShader  = createShader(gl, gl.FRAGMENT_SHADER, fragShaderSrc);
+    const program     = createProgram(gl, vertShader, fragShader);
 
     const posAttrLocation = gl.getAttribLocation(program, "a_position");
     const posBuffer = gl.createBuffer();
+
     gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
-
-    const vertices = new Float32Array([
-        -0.3, 0,
-        0, 0.5,
-        0.3, 0
-    ]);
-
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
     const vao = gl.createVertexArray();
     gl.bindVertexArray(vao);
     gl.enableVertexAttribArray(posAttrLocation);
-
-    const size       = 2;
-    const type       = gl.FLOAT;
-    const normalize  = false;
-    const stride     = 0;
-    const offset     = 0;
-
-    gl.vertexAttribPointer(posAttrLocation, size, type, normalize, stride, offset);
+    gl.vertexAttribPointer(posAttrLocation, 2, gl.FLOAT, false, 0, 0);
 
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
+    
     gl.useProgram(program);
-    gl.bindVertexArray(vao);
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    const resUniformLocation = gl.getUniformLocation(program, "u_resolution");
+    gl.uniform2f(resUniformLocation, gl.canvas.width, gl.canvas.height);
+
+    const colorUniformLocation = gl.getUniformLocation(program, "u_color");
+
+    for (let i = 0; i < 50; i++) {
+        setRectangle(gl, randomInt(300), randomInt(300), randomInt(300), randomInt(300));
+        gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+    }
 }
 
 main();
